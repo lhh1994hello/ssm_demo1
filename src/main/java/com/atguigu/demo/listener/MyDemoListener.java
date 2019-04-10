2b02899072073c99e981dccce2c6d29048688385
lhh <1497392219@qq.com>
2019-04-11 06:53:09 +0800

    监听器

---
+++ src/main/java/com/atguigu/demo/listener/MyDemoListener.java
@@ -0,0 +1,28 @@
+package com.atguigu.demo.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * @Author: lhh
+ * @Date: 2019/4/10 21:28
+ * @Version 1.0
+ */
+public class MyDemoListener implements ServletContextListener {
+    private static Logger logger = LoggerFactory.getLogger(MyDemoListener.class);
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("自定义监听器类contextInitialized方法...");
+        logger.info("自定义监听器类contextInitialized方法...");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("contextDestroyed...");
+        logger.info("自定义监听器类contextDestroyed方法...");
+    }
+}
