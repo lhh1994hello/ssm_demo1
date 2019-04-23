cf07672c18c76f4961b7d44f7c03e5362705c183
honghui_li <honghui_li@comwave.com.cn>
2019-04-23 19:34:14 +0800

    vue测试

--- src/main/java/com/atguigu/se/collect/map/HashMapDemo01.java
+++ src/main/java/com/atguigu/se/collect/map/HashMapDemo01.java
@@ -16,17 +16,39 @@ public class HashMapDemo01 {
     public void fun1() {
         Map<String, Object> map = new HashMap<String, Object>();
         Map<String, Object> table = new Hashtable<String, Object>();
-//        map.put(null, null);
-//        table.put("s", null);
-//        System.out.println(map);
-//        System.out.println(table);
+        String strTmp = "如鱼得水sd";
         map.put("a", "A");
         Object o = map.get("a");
+        map.remove("a");
         System.out.println(o);
 
+        int hashDemo = hashDemo(strTmp);
+        int hashCode = strTmp.hashCode();
+        System.out.println("hash值:" + hashDemo);
+        System.out.println("哈希码:" + Integer.toBinaryString(hashCode).length() + ":" + Integer.toBinaryString(hashCode));
+
+        double pow = Math.pow(2, 16);
+        System.out.println("次方：" + pow);
+        int i2 = 774889 >>> 16;
+        System.out.println("" + i2);
+        int i3 = 774889 ^ 709353;
+        System.out.println("异或运算：" + i3);
+
+        int andValue = 10 & 127;
+        System.out.println("andValue:"+andValue);
+
+
     }
-    static final int hash(Object key) {
-        int h;
-        return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
+
+    static final int hashDemo(Object key) {
+        int h = 0;
+        int hash = (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
+        System.out.println("key的hashcode:" + key.hashCode());
+        System.out.println("右移之后：" + (h >>> 16));
+        int i = 774889 ^ 11;
+        System.out.println("异或运算了：" + i);
+        return hash;
     }
+
+
 }
